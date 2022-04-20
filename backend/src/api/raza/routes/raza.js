6093d0dba2e9ebe4bb40da36759d1b66d8637cf8
potlitel <potlitel@gmail.com>
2022-04-19 21:52:59 -0400

    Se adiciona entidad Raza.

---
+++ backend/src/api/raza/routes/raza.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * raza router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::raza.raza');
---
+++ backend/src/api/raza/services/raza.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * raza service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::raza.raza');
