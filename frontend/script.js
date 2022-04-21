29098010670c042eba4c3971f50bb11d851140aa
potlitel <potlitel@gmail.com>
2022-04-21 13:20:12 -0400

    Se mustran elementos del API desde la UI principal del fichero index.html, además se comienza con el proceso de poblar de datos la BD.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,5 @@
 const APIURL = 'http://localhost:1337'
-let value;
-const CatsAPIURL = 'http://localhost:1337/api/cats/'
+const CatsAPIURLEndPoint = '/api/cats/'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
@@ -8,7 +7,7 @@ const main = document.getElementById('main')
 
 async function getUser(username) {
     try {
-        const {data} = await axios(CatsAPIURL+username+"?populate=raza,perfil_image")
+        const {data} = await axios(APIURL+CatsAPIURLEndPoint+username+"?populate=*")
         //data.bio = !!data.bio !== false ? data.bio : 'No description' ;
         console.info(data)
         createUserCard(data)
@@ -39,8 +38,13 @@ function createUserCard(user) {
              <img src="${APIURL}${user.data.attributes.perfil_image.data.attributes.url}" alt="${user.data.attributes.raza.data.attributes.nombre}" class="avatar">
             </div>
             <div class="user-info">
-                <h2>${user.data.attributes.raza.data.attributes.nombre}</h2>
-                <p>${user.bio}</p>
+                <h2>Raza: ${user.data.attributes.raza.data.attributes.nombre}</h2>
+                <p>Origen: ${user.data.attributes.origen.data.attributes.Pais}</p>
+                <p>Clasificación FIFE: ${user.data.attributes.clasificacion_fife.data.attributes.nombre}</p>
+                <p>Clima: ${user.data.attributes.clima.data.attributes.nombre}</p>
+                <p>Tamaño: ${user.data.attributes.tamano.data.attributes.valor}</p>
+                <p>Peso: ${user.data.attributes.peso.data.attributes.valor}</p>
+                <p>Esperanza de vida: ${user.data.attributes.esperanza_vida.data.attributes.valor}</p>
                 <ul>
                     <li>${user.followers} <strong>Followers</strong></li>
                     <li>${user.following} <strong>Following</strong></li>
@@ -73,6 +77,22 @@ function addReposToCard(repos) {
             repoEl.href = repo.html_url
             repoEl.target = '_blank'
             repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        
+    })
+
+}
+
+function addCaracteristicaToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos.slice(0, 12).forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            //repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.data.attributes.caracteristicas_fisicas.data.attributes.valor
 
             reposEl.appendChild(repoEl)
         
