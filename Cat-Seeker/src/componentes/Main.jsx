0b0eb93b3adcd477a45d9e6f8850dd2454c10f4c
potlitel <potlitel@gmail.com>
2022-04-25 22:12:28 -0400

    Se crea MainComponent.

---
+++ Cat-Seeker/src/componentes/Main.jsx
@@ -0,0 +1,15 @@
+import '../css/style.css'
+
+function App() {
+
+  return (
+      
+    <div>          
+        <form className="user-form" id="form">
+            <input type="text" id="search" placeholder='Busca una raza de gatos'/>
+        </form>      
+    </div>
+  )
+}
+
+export default App
